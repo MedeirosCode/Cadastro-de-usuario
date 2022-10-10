a3996139699b41d13148549d2d6a59ee36d197db
Christian Medeiros <christianmedeiros022@gmail.com>
2022-10-10 13:13:15 -0300

    first commit

---
+++ src/models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+  },
+  cpf: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    minlenght: 7,
+  },
+});
+
+// criando model
+
+const UserModel = mongoose.model("User", userSchema);
+module.exports = UserModel;
